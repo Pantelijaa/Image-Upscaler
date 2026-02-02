0c3274c6ec201b5d03dae9fe003ad6815660d5ab
Pantelijaa <nikolapan53@gmail.com>
2026-02-03 00:38:33 +0100

    Initial Commit

---
+++ Image Upscaler/Image Upscaler.cpp
@@ -0,0 +1,12 @@
+﻿// Image Upscaler.cpp : Defines the entry point for the application.
+//
+
+#include "Image Upscaler.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
