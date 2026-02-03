af70833346b3042507293d5672e8273e69213a61
Pantelijaa <nikolapan53@gmail.com>
2026-02-03 17:27:08 +0100

    Added test for Bilinear

--- Image Upscaler/Image Upscaler.cpp
+++ Image Upscaler/Image Upscaler.cpp
@@ -3,13 +3,19 @@
 
 #include <iostream>
 #include "core/Image.h"
-
-using namespace std;
+#include "core/Scaler.h"
+#include "interpolation/IInterpolator.h"
+#include "interpolation/Bilinear.h"
+const std::string PATH_TO_DATA = "../../../../data/";
 
 int main()
 {
 	Image img;
-	img.loadFromFile("../../../../data/input.jpg"); // PROMENITI NEKAKO U CMAKE
-	cout << "Hello CMake." << endl;
+	img.loadFromFile(PATH_TO_DATA + "/originals/input.jpg"); // PROMENITI NEKAKO U CMAKE
+
+	IInterpolator* it = new Bilinear();
+	Image upscaledImg = Scaler::upscale(img, img.getWidth() * 2, img.getHeight() * 2, *it);
+	upscaledImg.saveToFile(PATH_TO_DATA + "/results/output_bilinear.png");
+	std::cout << "Hello CMake." << std::endl;
 	return 0;
 }
