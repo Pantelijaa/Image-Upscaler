69bd6d3705a3ea98958a0e5a273492eda5de3ec4
Pantelijaa <nikolapan53@gmail.com>
2026-02-03 01:50:50 +0100

    Fix

--- Image Upscaler/core/Scaler.h
+++ Image Upscaler/core/Scaler.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "Image.h"
+#include "../interpolation/IInterpolator.h"
 
 class Scaler {
 	public:
-		static Image upscale(const Image& src, int nw, int nh, Interpolator* it);
+		static Image upscale(const Image& src, int nw, int nh, IInterpolator* it);
 }
