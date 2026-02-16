7affe0f60727b94748fbc82e8d5042c0983aeb6b
Pantelijaa <nikolapan53@gmail.com>
2026-02-16 15:52:33 +0100

    Added cubic weight

--- Image Upscaler/interpolation/Bicubic.cpp
+++ Image Upscaler/interpolation/Bicubic.cpp
@@ -1,5 +1,22 @@
-#include "Bicubic.h"
+﻿#include "Bicubic.h"
 
 Pixel Bicubic::interpolate(Image& img, float x, float y) {
-	return Pixel{ 0, 0, 0 }; // Placeholder implementation
+	return Pixel{ 0, 0, 0 };
+}
+
+double Bicubic::cubic_weight(double x) {
+	constexpr double a = -0.5;
+	double abs_x = std::abs(x);
+	if (abs_x <= 1.0) {
+		return (a + 2.0) * abs_x * abs_x * abs_x
+			    - (a + 3.0) * abs_x * abs_x
+			    + 1.0;
+	}
+	else if (abs_x < 2.0) {
+		return a * abs_x * abs_x * abs_x
+				- 5.0 * a * abs_x * abs_x
+				+ 8.0 * a * abs_x
+				- 4.0 * a;
+	}
+	return 0.0;
 }
--- Image Upscaler/interpolation/Bicubic.h
+++ Image Upscaler/interpolation/Bicubic.h
@@ -4,4 +4,6 @@
 class Bicubic : public IInterpolator {
 public:
 	Pixel interpolate(Image& image, float x, float y) override;
+private:
+	static double cubic_weight(double x);
 };
