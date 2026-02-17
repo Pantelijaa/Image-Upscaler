c1b9a393cabfd28d6a1fe980daff75b6f9b10758
Pantelijaa <nikolapan53@gmail.com>
2026-02-17 15:47:49 +0100

    Images are now augumented to increase dataset

--- CNN/SRDataset.py
+++ CNN/SRDataset.py
@@ -24,28 +24,50 @@ class SRDataset(Dataset):
 			if img is None:
 				continue
 
-			h, w = img.shape
-			
-			w -= w % self.scale
-			h -= h % self.scale
-			img = img[:h, :w]
+			augumented = self._augument(img)
 
-			# normalize to [0, 1]
-			hr = img.astype(np.float32) / 255.0
+			for augumented_image in augumented
+				self._extract_patches(augumented_image, stride)
 
-			lr = cv2.resize(hr, (w // self.scale, h // self.scale), interpolation=cv2.INTER_CUBIC)
-			lr = cv2.resize(lr, (w, h), interpolation=cv2.INTER_CUBIC)
+		
+	def _augument(self, img):
+		"""
+		Generate 8  augumented versions of image 
+		(original		+ 3 rotations + 
+		horizontal flip + 3 flipped rotations)
+		"""
+		variants = []
+		for rot in range(4):
+			rotated = np.rot90(img, k=rot)
+			variants.append(rotated)
+			variants.append(np.fliplr(rotated))
+		return variants
 
-			# extract patches
-			for y in range(0, h - self.patch_size + 1, stride):
-				for x in range(0, w - self.patch_size + 1, stride):
-					hr_patch = hr[y:y+ self.patch_size, x:x + self.patch_size]
-					lr_patch = lr[y:y + self.patch_size, x:x + self.patch_size]
+	def _extract_patches(self, img, stride):
+		h, w = img.shape
+		w -= w % self.scale
+		h -= h % self.scale
+		img = img[:h, :w]
 
-					# tensor shape: (1, H, W)
-					self.patches_hr.append(torch.from_numpy(hr_patch).unsqueeze(0))
-					self.patches_lr.append(torch.from_numpy(lr_patch).unsqueeze(0))
+		if h < self.patch_size or w < self.patch_size:
+			return
 
+		# normalize to [0, 1]
+		hr = img.astype(np.float32) / 255.0
+
+		lr = cv2.resize(hr, (w // self.scale, h // self.scale), interpolation=cv2.INTER_CUBIC)
+		lr = cv2.resize(lr, (w, h), interpolation=cv2.INTER_CUBIC)
+
+		# extract patches
+		for y in range(0, h - self.patch_size + 1, stride):
+			for x in range(0, w - self.patch_size + 1, stride):
+				hr_patch = hr[y:y+ self.patch_size, x:x + self.patch_size]
+				lr_patch = lr[y:y + self.patch_size, x:x + self.patch_size]
+
+				# tensor shape: (1, H, W)
+				self.patches_hr.append(torch.from_numpy(hr_patch).unsqueeze(0))
+				self.patches_lr.append(torch.from_numpy(lr_patch).unsqueeze(0))
+	
 	def __len__(self):
 		return len(self.patches_lr)
 
