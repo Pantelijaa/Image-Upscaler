012ce53e0193adc8e435c493ddf70d3f299db080
Pantelijaa <nikolapan53@gmail.com>
2026-02-17 15:38:53 +0100

    Increased learning rate and added residual to SRCNN

--- CNN/SRCNN.py
+++ CNN/SRCNN.py
@@ -5,13 +5,14 @@ class SRCNN(nn.Module):
 	def __init__(self):
 		super(SRCNN, self).__init__()
 
-		self.conv1 = nn.Conv2d(1, 64, kernel_size=9, padding=4)
-		self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=2)
-		self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
-		self.relu = nn.ReLU(inplace=False)
+		self.conv1 = nn.Conv2d(1, 128, kernel_size=9, padding=4)
+		self.conv2 = nn.Conv2d(128, 64, kernel_size=5, padding=2)
+		self.conv3 = nn.Conv2d(64, 1, kernel_size=5, padding=2)
+		self.relu = nn.ReLU(inplace=True)
 
 	def forward(self, x):
-		x = self.relu(self.conv1(x))
-		x = self.relu(self.conv2(x))
-		x = self.conv3(x)
-		return x
+		relsidual = x
+		out = self.relu(self.conv1(x))
+		out = self.relu(self.conv2(out))
+		out = self.conv3(out)
+		return out + residual
--- CNN/train_cnn.py
+++ CNN/train_cnn.py
@@ -8,10 +8,10 @@ import os
 import time
 from export_onnx import export_onnx
 
-def train(data_dir, train_minutes = 3, batch_size=128, lr=1e-4, val_split=0.2, scale=2):
+def train(data_dir, train_minutes = 3, batch_size=64, lr=1e-3, val_split=0.2, scale=2):
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 	print("Loading dataset...", flush=True)
-	dataset = SRDataset(data_dir, patch_size=33, scale=scale, stride=14)
+	dataset = SRDataset(data_dir, patch_size=33, scale=scale, stride=7)
 	val_size = int(len(dataset) * val_split)
 	train_size = len(dataset) - val_size
 	train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
